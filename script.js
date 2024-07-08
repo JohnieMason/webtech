bd0893b919c687bdb0bc0995f20a4d095c4f9410
Johnie Mason <johniemason@students.uonbi.ac.ke>
2024-07-08 13:25:52 +0300

    Javascript basics

---
+++ script.js
@@ -0,0 +1,62 @@
+///Declaring variables = 
+x = 10;
+var y = 15;
+let course = "web development"
+const pi = 3.1452;
+
+///How to display output of variables
+console.log(x);
+
+///comparison operator
+// greater than >
+// less than <
+// equal to ==
+// equal value and equal data type ===
+// greater than or equal to >=
+// less than or equal to <=
+console.log(x < y);
+console.log(x > y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x === y);
+
+
+///division
+console.log(x / y);
+
+
+///addition
+console.log(x + y);
+
+
+///subtraction
+console.log (y - x);
+
+
+
+///multiplication
+console.log(x * y);
+
+//increment or decrement
+console.log( ++x);
+console.log( --x);
+
+/// condition  ? expression 1 : expression 2
+console.log( x > y ? 'Yes': 'No') ;
+
+// numbers : whole integers or decimals -
+// strings : characteristics
+// bolean : true or false
+// array  : 
+let fruits = ["apples", "oranges", "berries"];
+console.log( fruits );
+
+//undefined data type
+let subject;
+
+//function 
+
+
+
+
